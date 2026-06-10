b3fad877f5036184254537ba9fdf8ebda3259568
Ander <20172991@aloe.ulima.edu.pe>
2026-06-10 18:54:33 -0500

    base

--- src/test/java/Semana10/Ejercicio2/Validador.java
+++ src/test/java/Semana10/Ejercicio2/Validador.java
@@ -2,12 +2,12 @@
 package Semana10.Ejercicio2;
 
 public class Validador {
-
+    
     public boolean validarCorreoElectronico(String correo) {
         return correo.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
     }
-
+    
     public boolean validarNumeroTelefono(String numero) {
-        return numero.matches("\\d{10}");
+        return numero.matches("\\d{9}");
     }
 }
